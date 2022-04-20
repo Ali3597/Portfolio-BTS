f840a02e432ffb95f65543c5eb09c10e215a5dba
Ali <ali_saleh-4@hotmail.fr>
2022-04-20 06:05:53 +0200

    add context login

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ThemeContextProvider } from "./context/ThemeContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <App />
-    </ThemeContextProvider>
+    <AuthContextProvider>
+      <ThemeContextProvider>
+        <App />
+      </ThemeContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,26 +6,19 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaMailBulk } from "react-icons/fa";
 import { useThemeContext } from "../../hooks/useThemeContext";
 import { useCollection } from "../../hooks/useCollection";
-
-const Educations = [
-  {
-    title: "Ecole d'ingenierie EPSI",
-    beginDate: "2020",
-    endDate: "En cours",
-    location: "Bordeaux France",
-  },
-  {
-    title: "Arrivée en licence 3 de Mathematique",
-    beginDate: "2014",
-    endDate: "2018",
-    location: "Université de Bordeaux",
-  },
-];
+import { useEffect } from "react";
 
 export function Home() {
   const { theme } = useThemeContext();
   const { documents: interests } = useCollection("interests");
   const { documents: studies } = useCollection("studies");
+  const { documents: homeParagraphes } = useCollection("homeParagraphes");
+  useEffect(() => {
+    if (homeParagraphes) {
+      console.log(homeParagraphes, "zzzz");
+    }
+  }, [homeParagraphes]);
+
   console.log(theme);
   return (
     <div
@@ -53,26 +46,13 @@ export function Home() {
       </div>
       <div className="right-home">
         <h1 style={{ color: theme.greyTitleColor }}>Biographie</h1>
-        <p style={{ color: theme.basicColor }}>
-          Je suis Saleh Ali, étudiant en deuxième année de BTS SIO (Services
-          Informatiques aux Organisations), Option SLAM (Solutions Logicielles
-          et Application Métier) l'option spécialisée pour le développement à
-          l'école d'ingenierie informatique Epsi de Bordeaux
-        </p>
-        <p style={{ color: theme.basicColor }}>
-          J'ai un parcours assez atypique car en effet après l'obtention de mon
-          baccalauréat je me suis dirigé vers une licence de mathématique ou je
-          suis suis arrivé en licence 3 avant d'arrêter, car je ne me plaisais
-          pas dans mes etudes ,pour rentrer dans le monde proffesionnel, pendant
-          Deux avants avenat de reprendre les etudes en informatqieu a l'ecolde
-          d'ingenierie ESI de Bordeaux.
-        </p>
-        <p style={{ color: theme.basicColor }}>
-          Mon projet professionnel est de poursuivre mes trois porchaines années
-          études après l'obtention de mon BTS en alternance eventuellment en
-          developpement Web , afin d'acqueir l'experience necessaire a mon
-          entrée dans le monde proffesionel
-        </p>
+        {homeParagraphes &&
+          homeParagraphes.map((para) => (
+            <p key={para.id} style={{ color: theme.basicColor }}>
+              {para.details}
+            </p>
+          ))}
+
         <div className="interest">
           <div className="interest-left">
             <h3 style={{ color: theme.greyTitleColor }}>Interêts</h3>
@@ -86,26 +66,30 @@ export function Home() {
           <div>
             <h3 style={{ color: theme.greyTitleColor }}>Etudes</h3>
             <ul className="interest-ul" style={{ color: theme.basicColor }}>
-              {Educations.map((educ, index) => (
-                <li key={index}>
-                  <span>
-                    <FaGraduationCap size={30} />
-                  </span>
-                  <div className="interest-lis">
-                    <div className="up-li">{educ.title}</div>
-                    <p
-                      style={{ color: theme.date }}
-                      style={{ color: theme.date }}
-                    >
-                      {" "}
-                      {educ.beginDate} - {educ.endDate}
-                    </p>
-                    <div style={{ color: theme.date }} className="down-li">
-                      {educ.location}
+              {studies &&
+                studies.map((educ, index) => (
+                  <li key={index}>
+                    <span>
+                      <FaGraduationCap size={30} />
+                    </span>
+                    <div className="interest-lis">
+                      <div className="up-li">{educ.title}</div>
+                      <p
+                        style={{ color: theme.date }}
+                        style={{ color: theme.date }}
+                      >
+                        {" "}
+                        {educ.start.toDate().toLocaleDateString("fr")} -{" "}
+                        {educ.end
+                          ? educ.end.toDate().toLocaleDateString("fr")
+                          : "En cours"}
+                      </p>
+                      <div style={{ color: theme.date }} className="down-li">
+                        {educ.location}
+                      </div>
                     </div>
-                  </div>
-                </li>
-              ))}
+                  </li>
+                ))}
             </ul>
           </div>
         </div>
